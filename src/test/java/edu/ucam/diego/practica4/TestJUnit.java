e2209c426f660c9f68776f09a732ff8bf1225e00
diegostreetbob <d761017@hotmail.com>
2020-12-19 09:08:38 +0100

    .

---
+++ src/test/java/edu/ucam/diego/practica4/TestJUnit.java
@@ -0,0 +1,21 @@
+package edu.ucam.diego.practica4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestJUnit {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+	@Test
+	public void test_getArea() {
+		Rectangulo rect = new Rectangulo(3,3);
+		double areareal = rect.getArea();
+		double areaesperada=9.0;
+		assertEquals(areaesperada, areareal,0);
+	}
+
+}
