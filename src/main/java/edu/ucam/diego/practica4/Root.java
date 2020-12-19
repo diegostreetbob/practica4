5fc947f84f856d7933c59edbdef496a4c7e027ae
diegostreetbob <d761017@hotmail.com>
2020-12-19 07:17:17 +0100

    .

--- src/main/java/edu/ucam/diego/practica4/Root.java
+++ src/main/java/edu/ucam/diego/practica4/Root.java
@@ -5,9 +5,6 @@ public class Root {
 			Poligono pol = new Rectangulo(3,4);
 			System.out.println("Tengo " + pol.getLados() + " lados");
 			System.out.println("Area:"+pol.getArea());
-			//
-			pol=new Triangulo(2,2);
-			System.out.println("Tengo " + pol.getLados() + " lados");
-			System.out.println("Area:"+pol.getArea());			
+	
 	}
 }
